c2aba426300c37c48ad057ebdc288f16049291ad
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-20 00:26:47 -0300

    multi line strings

---
+++ day-01/multi-line-strings.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// exibi a quebra de linha no código final
+
+var string1 = """
+    primeira linha
+    segunda linha
+    terceira linha
+"""
+
+// não exibi a quebra de linha no código final
+
+var string2 = """
+    primeira linha 1 \
+    segunda linha 2 \
+    terceira linha 3 
+"""
