4caba748b4c0ffe3878c832a34ef4e29d0524eb8
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-22 19:58:20 -0300

    tuples

---
+++ day-02/tuples.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+// valores fixos
+var profile = (first: "Alexandre", last: "Bitar", age: 21)
+
+print(profile.first) // "Alexandre"
+print(profile.last) // "Bitar"
+print(profile.age) // 21
+
+// busca por posição númerica
+print(profile.0) // "Alexandre"
