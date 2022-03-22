e4a6081b717d7da26e7c9fb78b4f872904f3b7e1
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-22 19:57:33 -0300

    arrays

---
+++ day-02/arrays.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let steve = "Steve Jobs"
+let alexandre = "João Alexandre"
+let antonio = "Antonio Assaid"
+let pedro = "João Pedro"
+
+let user = [steve, alexandre, antonio, pedro]
+
+print(user) // ["Steve Jobs", "João Alexandre", "Antonio Assaid", "João Pedro"]
+print(user[2]) // Antonio Assaid
