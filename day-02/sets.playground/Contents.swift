99d5df7708a3846b5269bb9e1f6cf82eb169a2ce
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-22 19:58:09 -0300

    sets

---
+++ day-02/sets.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+// ordem não definida
+ 
+let names = Set(["Gabriel", "Marcelo", "Julio", "Fernanda", "Gabriel"]) // valores duplicados não são considerados
+
+print(names) // ["Marcelo", "Julio", "Gabriel", "Fernanda"]
