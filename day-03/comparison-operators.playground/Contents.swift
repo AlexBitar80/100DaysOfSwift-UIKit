c7bfbbe28b2ae589c541faaffe2373eeaa001a61
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-23 20:44:36 -0300

    comparison operators

---
+++ day-03/comparison-operators.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let value1 = 12
+let value2 = 23
+
+value1 == value2 // false
+
+value1 > value2 // false
+ 
+value1 <= value2 // true
+  
+value1 >= value2 // false
+
+value1 != value2 // true
