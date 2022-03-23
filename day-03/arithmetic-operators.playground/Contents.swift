60c7e0da9a4a5d32657245624698f0461b256399
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-23 20:44:04 -0300

    arithmetic operators

---
+++ day-03/arithmetic-operators.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let score = 32
+let finalScore = 12
+
+// soma e subtração
+let total = score + finalScore
+let difference = score - finalScore
+
+// multiplicação e divisão
+let product = score * finalScore
+let divided = score / finalScore
+
+// resto da divisão
+let remainder = 5 % finalScore
+
+print(total)
+print(difference)
+print(product)
+print(divided)
+print(remainder)
