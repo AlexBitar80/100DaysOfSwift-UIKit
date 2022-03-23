1ab428aa891f1650ff0982669dba23b0198fb59a
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-23 20:45:11 -0300

    operator overloading

---
+++ day-03/operator-overloading.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let sum = 32 + 32 // 64
+
+let fruit = ["bacana", "apple", "Cherry"]
+let otherFruits = ["Melon", "Orange", "Grape"]
+
+let allFruits = fruit + otherFruits // união de dois arrays
