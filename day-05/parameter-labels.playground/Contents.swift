5fa1f98c5ef32faeb64bc5241f6a7df49461ad45
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-25 21:41:35 -0300

    parameters-labels

---
+++ day-05/parameter-labels.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// # Rótulos de parâmetros
+
+// O primeiro rótulo será usado na chamada da função e o segundo será usado dentro da função
+func sayHello(to name: String) {
+    print("Hello, \(name)!")
+}
+
+sayHello(to: "João Alexandre")
