366daf02538b356ca02647e6272cc242e0d7520f
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-25 21:45:50 -0300

    returning-values

---
+++ day-05/returning-values.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// # Retornando valores
+
+func square(number: Int) -> Int { // retorna um número inteiro
+    return number * number
+}
+
+let result = square(number: 8)
+print(result)
