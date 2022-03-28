bbd31d74ce0ea8032573a08ef1b3b9e1fab1ea72
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-27 21:31:35 -0300

    closures-with-multiple-parameters

---
+++ day-07/closures-with-multiple-parameters.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// # Fechamentos com vários parâmetros
+
+func travel(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived!")
+}
+
+travel { // recebendo os dois paråmetros
+    "I'm going to \($0) at \($1) miles per hour."
+}
