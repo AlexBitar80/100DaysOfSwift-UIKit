318b142edda0cbd987d0c3e8062034fcf8dd5a52
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-28 22:35:47 -0300

    properties-and-methods-of-strings

---
+++ day-08/properties-and-methods-of-strings.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let string = "os ogros tem camadas"
+
+print(string.count)
+
+print(string.uppercased())
+
+print(string.hasPrefix("os"))
+
+print(string.sorted()) // considera espaços vazios
