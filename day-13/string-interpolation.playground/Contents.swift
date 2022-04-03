26de4e932c89cb53b96ee712f38cc88eb195586d
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-03 01:27:53 -0300

    string-interpolation

---
+++ day-13/string-interpolation.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// # String interpolation
+
+var name: String = "João Alexandre"
+var age: Int = 21
+var height: Double = 1.70
+
+print("His name is \(name), his age is \(age) years old, his height is \(height)")
+
+print("His name is \(name), his age is \(age * 2) years old, his height is \(height)")
