6d0b5371e7f1144e38466f90e55dd308da8e73e4
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-03 01:27:45 -0300

    operators

---
+++ day-13/operators.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+// # operators
+
+var a = 10
+a = a + 1
+a = a - 3
+a = a * 7
+
+var b = 20
+b += 3
+b -= 5
+
+var c = 3.3
+var d = 2.3
+var e = c + d
+
+var name1 = "João"
+var name2 = "Alexandre"
+
+var result = name1 + " and " + name2
+
+var f = 2.3
+var g = 2.5
+var h = f + g
+
+h > 4.8
+h < 4.8
+h >= 4.0
+h <= 4.8
+
+var secondName = "Gabriel"
+
+secondName == "Gabriel"
+secondName != "Gabriel"
+
+
+var isOpen = true
+isOpen
+!isOpen
