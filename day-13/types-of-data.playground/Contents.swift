43e5f200e6c66ecc7d7860fd9bd43d02f2669b96
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-03 01:28:13 -0300

    types-of-data

---
+++ day-13/types-of-data.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// # Tipos de dados
+
+
+// String e Int
+
+var name: String = "Alexandre"
+var age: Int = 21
+
+// Float e Double
+
+var latitude: Double = 23.404588584
+var longitude: Float = -3884.0023444
+
+// # Bool
+
+var isLogged: Bool
+isLogged = true
+
+// var name = "Alexandre"
+// var age = 21
+// var latitude = 23.404588584
+// var longitude = -3884.0023444
+// var isLogged = true
