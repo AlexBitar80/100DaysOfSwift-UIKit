8e6335f39acd765daa1ba20f8fa110eb293b42ae
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-03 01:27:03 -0300

    arrays

---
+++ day-13/arrays.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// # arrays
+
+var names = ["Alexandre", "Gabriel", "Pedro", "Antonio"]
+
+names[0]
+names[1]
+names[2]
+names[3]
+
+type(of: names)
+
+var animals: [String] = ["Cobra", "Girrafa", "Coelho", "Sapo"]
+
+type(of: animals)
+
+var values: [Any] = ["Valor1", "Valor2", 3]
+
+type(of: values)
+
+var names1 = ["pessoa1", "pessoa2"]
+var names2 = ["pessoa3", "pessoa4"]
+
+var value2 = names1 + names2
