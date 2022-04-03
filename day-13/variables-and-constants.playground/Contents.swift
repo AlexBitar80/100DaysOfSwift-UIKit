60e8e513f6caaca0a02fad48d93bfcee49963a31
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-03 01:28:32 -0300

    variables-and-constants

---
+++ day-13/variables-and-constants.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// # Variáveis e constantes
+
+var name = "Alexandre"
+
+let lastName = "Bitar"
+
+name = "João Alexandre"
+lastName = "Bitar de Andrade" // Não pode ter seu valor alterado
