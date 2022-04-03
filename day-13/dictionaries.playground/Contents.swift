b48e36aad0da837b542238cc939c99be4b76dfed
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-03 01:27:33 -0300

    dictionaries

---
+++ day-13/dictionaries.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+// # Dicionarios
+
+var person = ["first": "Taylor", "middle": "Alison", "last": "Swift", "month": "December", "website": "taylorswift.com"]
+
+person["middle"]
+person["month"]
