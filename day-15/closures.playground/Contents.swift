54b488fda3a7f9e234594e898783a06eb5512a28
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-05 23:51:20 -0300

    closures

---
+++ day-15/closures.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//MARK: - Fechamentso
+
+let vw = UIView()
+
+UIView.animate(withDuration: 0.5, animations: {
+    vw.alpha = 0
+})
+
+//MARK: - Fechamentos à direita
+
+let view = UIView()
+
+UIView.animate(withDuration: 0.5) {
+    view.alpha = 0
+}
