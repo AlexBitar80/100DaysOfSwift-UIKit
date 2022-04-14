0f7c558ccaa4acfb5a2978aa56ab72903fb714f7
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-04-14 13:51:47 -0300

    fixed variables and added label with choices

---
+++ project2/project2/ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by João Alexandre on 31/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var questionsAnswered = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .pause, target: self, action: #selector(totalScore))
+        
+        countries += [
+            "estonia",
+            "france",
+            "germany",
+            "ireland",
+            "italy",
+            "monaco",
+            "nigeria",
+            "poland",
+            "russia",
+            "spain",
+            "uk",
+            "us"
+        ]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion(action: nil)
+    }
+    
+    func askQuestion(action: UIAlertAction!) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = "pontuação atual \(score)"
+        
+        titleLabel.text = "Selecione a bandeira de \(countries[correctAnswer].uppercased())"
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correto"
+            score += 1
+        } else {
+            title = """
+                Resposta errada!
+            
+                Está é a bandeira de
+                \(countries[sender.tag].uppercased()) :(
+            """
+            score -= 1
+        }
+        
+        questionsAnswered += 1
+        
+        if questionsAnswered < 10 {
+            let ac = UIAlertController(title: title, message: "Sua pontuação agora é de \(score).", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Continuar", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: title, message: "Sua pontuação final é de \(score). Parabéns!", preferredStyle: .alert)
+        
+            ac.addAction(UIAlertAction(title: "Tentar novamente", style: .default, handler: askQuestion))
+                         
+            present(ac, animated: true)
+            
+            score = 0
+            questionsAnswered = 0
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @objc func totalScore() {
+        let currentScore = UIAlertController(title: "pontuação atual \(score)", message: "", preferredStyle: .alert)
+        
+        currentScore.addAction(UIAlertAction(title: "Fechar", style: .default, handler: nil))
+        
+        present(currentScore, animated: true)
+    }
+}
